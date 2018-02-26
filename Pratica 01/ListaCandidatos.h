bda72154ff39cbae11feb09ce3dd5e5811fd80df
TheresaMancini <theresa.mancini@hotmail.com>
2018-02-25 21:11:39 -0300

    ListaCandidatos(), int size, void tamanho, void adicionacomoHead

--- Pratica 01/ListaCandidatos.h
+++ Pratica 01/ListaCandidatos.h
@@ -1,13 +1,25 @@
 class ListaCandidatos {
 public:
     NoCandidato *head;
+    int size;
 
     bool estaVazia(){
         return head==NULL;
     }
 
     ListaCandidatos(){
-            // Head começa com null
+        this->head=NULL;
+	this->size=0;
+    }
+    int tamanho(){
+    	return this->size;
+    }
+    void adicioneComoHead(Candidato *c){
+	 NoCandidato* m = NULL;
+	 m = new NoCandidato(c,NULL);
+         this->head=m;
+         this->head->next=head;
+         this->size=size+1;
     }
 
 }
